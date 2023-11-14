9f46b216557c788dc473e37b58bb00919696520b
mahvec <62505213+mahvec@users.noreply.github.com>
2023-11-14 14:31:11 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import background from "../src/assets/back.svg";
+import "./App.css";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
